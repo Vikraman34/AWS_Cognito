ca3b62bf00bd50bdaedbdf653e819dfa0257e100
Vikraman <vikraman18195@cse.ssn.edu.in>
2022-08-09 02:27:55 +0530

    login fix

--- demosite/product/views.py
+++ demosite/product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import boto3
 
 def index(request):
@@ -6,26 +6,28 @@ def index(request):
 
 def login(request):
     if request.method == 'POST':
-        username = str(request.POST['username'])
-        password = str(request.POST['pass1'])
-        print(username, password)
-        client = boto3.client('cognito-idp', 'ap-south-1')
-        response = client.initiate_auth(
-            ClientId='777fktanjr9jc0d7brvp66vego',
-            AuthFlow='USER_PASSWORD_AUTH',
-            AuthParameters={
-                'USERNAME': username,
-                'PASSWORD': password
-            }
-        )
-        print(response)
-        print('AccessToken', response['AuthenticationResult']['AccessToken'])
-        print('RefreshToken', response['AuthenticationResult']['RefreshToken'])
-        request.session['id_token'] = response['AuthenticationResult']['IdToken']
+        if str(request.POST['username']) != '':
+            username = str(request.POST['username'])
+            password = str(request.POST['pass1'])
+            print(username, password)
+            client = boto3.client('cognito-idp', 'ap-south-1')
+            response = client.initiate_auth(
+                ClientId='777fktanjr9jc0d7brvp66vego',
+                AuthFlow='USER_PASSWORD_AUTH',
+                AuthParameters={
+                    'USERNAME': username,
+                    'PASSWORD': password
+                }
+            )
+            print(response)
+            print('AccessToken', response['AuthenticationResult']['AccessToken'])
+            print('RefreshToken', response['AuthenticationResult']['RefreshToken'])
+            request.session['id_token'] = response['AuthenticationResult']['IdToken']
+            return redirect(home)
     return render(request, "login.html")
 
 def home(request):
     id_token = request.session.get('id_token')
-    print(id_token)
+    print(id_token, "hi")
     return render(request, "home.html")
 
